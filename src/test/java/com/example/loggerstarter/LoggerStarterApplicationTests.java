43a8cee638e42beec3e2a04862771725fa57ec69
Microd <Microd@inbox.ru>
2024-07-17 22:43:01 +0500

    First

---
+++ src/test/java/com/example/loggerstarter/LoggerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggerstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
